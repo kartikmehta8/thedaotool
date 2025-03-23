91bbd14e6698811429f816f1ded791db7ee5c92a
Kartik Mehta <kartikmehta202@gmail.com>
2025-03-23 10:49:14 +0530

    dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar } from "../components";
+import { Layout } from "antd";
+import BusinessDashboard from "./business/BusinessDashboard";
+import ContractorDashboard from "./contractor/ContractorDashboard";
+
+const { Content } = Layout;
+
+const Dashboard = ({ user }) => {
+  const role = user?.role;
+
+  return (
+    <Layout style={{ minHeight: "100vh", backgroundColor: "#141414" }}>
+      <Navbar email={user.email} />
+      <Content style={{ padding: "2rem" }}>
+        {role === "business" && <BusinessDashboard />}
+        {role === "contractor" && <ContractorDashboard />}
+        {!role && (
+          <div style={{ color: "#fff" }}>
+            Unable to determine your role. Please contact support.
+          </div>
+        )}
+      </Content>
+    </Layout>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import Dashboard from "./Dashboard";
+import Landing from "./Landing";
+import BusinessDashboard from "./business/BusinessDashboard";
+import ContractorDashboard from "./contractor/ContractorDashboard";
+import BusinessProfile from "./business/BusinessProfile";
+import ContractorProfile from "./contractor/ContractorProfile";
+
+export {
+  Login,
+  Signup,
+  Dashboard,
+  Landing,
+  BusinessDashboard,
+  ContractorDashboard,
+  BusinessProfile,
+  ContractorProfile,
+};
