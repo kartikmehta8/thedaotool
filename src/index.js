84cb2d01539b834f36531a03dfd4e6ffa8f47f77
Kartik Mehta <kartikmehta202@gmail.com>
2025-03-23 10:47:58 +0530

    index and app configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ConfigProvider, theme } from "antd";
+import "antd/dist/reset.css";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "Reddit Sans, sans-serif",
+        },
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <App />
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+    </ConfigProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
