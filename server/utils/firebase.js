2e627a4385e654cbe91f23999d6e56f1ae0c0855
Kartik Mehta <kartikmehta202@gmail.com>
2025-05-17 17:58:55 +0530

    Use Firebase Admin and Service account, remove firebase client SDK

--- server/utils/firebase.js
+++ server/utils/firebase.js
@@ -1,24 +1,18 @@
-const { initializeApp } = require('firebase/app');
-const { getAuth } = require('firebase/auth');
-const { getFirestore } = require('firebase/firestore');
-const { getDatabase } = require('firebase/database');
+const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MSG_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  databaseURL: process.env.FB_REALTIME_DATABASE,
-};
+const serviceAccount = require('../firebase.json');
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const rtdb = getDatabase(app);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FB_REALTIME_DATABASE,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+});
+
+const auth = admin.auth();
+const db = admin.firestore();
+const rtdb = admin.database();
 
 module.exports = { auth, db, rtdb };
