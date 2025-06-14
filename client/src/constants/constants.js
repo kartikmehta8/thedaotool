50cee98d3643c6dc0150ea9af5199ec0af30c711
Kartik Mehta <kartikmehta202@gmail.com>
2025-06-14 17:01:05 +0530

    Privy Client Configuration

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,3 +1,5 @@
 export const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api`;
 export const USER_KEY = 'payman-user';
 export const TOKEN_KEY = 'token';
+export const APP_ID = process.env.REACT_APP_PRIVY_APP_ID;
+export const APP_CLIENT_ID = process.env.REACT_APP_PRIVY_CLIENT_ID;
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import './index.css';
 import { Toaster } from 'react-hot-toast';
 import ThemeProvider, { useTheme } from './context/ThemeContext';
 
+import { PrivyProvider } from '@privy-io/react-auth';
+import { APP_ID, APP_CLIENT_ID } from './constants/constants';
+
 const Root = () => {
   const { algorithm } = useTheme();
   return (
@@ -28,7 +31,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Root />
+      <PrivyProvider
+        appId={APP_ID}
+        clientId={APP_CLIENT_ID}
+        config={{
+          embeddedWallets: {
+            ethereum: {
+              createOnLogin: 'off',
+            },
+          },
+        }}
+      >
+        <Root />
+      </PrivyProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
