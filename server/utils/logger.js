319d8fea8429c4e1b34925964a5c929c766f7da2
Kartik Mehta <kartikmehta202@gmail.com>
2025-06-21 15:06:08 +0530

    Update loki logger

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,26 +1,32 @@
 const pino = require('pino');
 const config = require('../config/loggerConfig');
 
-const transport =
-  config.logToLoki && config.lokiUrl
-    ? pino.transport({
-        target: 'pino-loki',
-        options: {
-          host: config.lokiUrl,
-          batching: true,
-          interval: 5,
-          silenceErrors: false,
-          labels: { service: 'bizzy-backend' },
-        },
-      })
-    : undefined;
+let logger;
 
-const logger = pino(
-  {
-    level: config.logLevel,
+if (config.logToLoki && config.lokiUrl) {
+  const transport = pino.transport({
+    target: 'pino-loki',
+    options: {
+      host: config.lokiUrl,
+      labels: { service: 'bizzy-backend' },
+      batching: true,
+      interval: 5,
+      silenceErrors: false,
+    },
+  });
+
+  logger = pino(
+    {
+      level: config.logLevel || 'info',
+      timestamp: pino.stdTimeFunctions.isoTime,
+    },
+    transport
+  );
+} else {
+  logger = pino({
+    level: config.logLevel || 'info',
     timestamp: pino.stdTimeFunctions.isoTime,
-  },
-  transport || pino.destination(1)
-);
+  });
+}
 
 module.exports = logger;
